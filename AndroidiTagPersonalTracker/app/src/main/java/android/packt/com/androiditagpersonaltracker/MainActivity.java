50292d7b40a761d7a136be0cce19002d730209eb
Madhur Bhargava <madhur.bhargava333@gmail.com>
2017-06-21 11:05:59 +0200

    Added AndroidiTagPersonalTracker Code

---
+++ AndroidiTagPersonalTracker/app/src/main/java/android/packt/com/androiditagpersonaltracker/MainActivity.java
@@ -0,0 +1,248 @@
+package android.packt.com.androiditagpersonaltracker;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "PacktBLEiTag";
+    private static final String NAME_iTAG = "itag";
+
+    ListView deviceListView;
+    Button startScanningButton;
+    Button stopScanningButton;
+
+    ArrayAdapter<String> listAdapter;
+    ArrayList<BluetoothDevice> deviceList;
+
+    BluetoothManager bluetoothManager;
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothLeScanner bluetoothLeScanner;
+    BluetoothGatt bluetoothGatt;
+
+    Timer timer;
+    ArrayList<Double> distanceList = new ArrayList<>();
+
+    private final static int MAX_DISTANCE_VALUES = 10;
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        deviceListView = (ListView) findViewById(R.id.deviceListView);
+        startScanningButton = (Button) findViewById(R.id.startScanButton);
+        stopScanningButton = (Button) findViewById(R.id.stopScanButton);
+        stopScanningButton.setVisibility(View.INVISIBLE);
+
+        listAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1);
+        deviceList = new ArrayList<>();
+        deviceListView.setAdapter(listAdapter);
+        startScanningButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startScanning();
+            }
+        });
+        stopScanningButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                stopScanning();
+            }
+        });
+
+        initialiseBluetooth();
+
+        deviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                stopScanning();
+                listAdapter.clear();
+                BluetoothDevice device = deviceList.get(position);
+                device.connectGatt(MainActivity.this, true, gattCallback);
+
+            }
+        });
+
+        if (bluetoothAdapter != null && !bluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BT);
+        }
+
+        // Make sure we have access coarse location enabled, if not, prompt the user to enable it
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("This app needs location access");
+            builder.setMessage("Please grant location access so this app can detect peripherals.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.show();
+        }
+    }
+
+    // Device scan callback.
+    private ScanCallback leScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            if(result.getDevice() != null) {
+                if(!isDuplicate(result.getDevice())) {
+                    synchronized (result.getDevice()) {
+                        if(result.getDevice().getName() != null && result.getDevice().getName().toLowerCase().contains(NAME_iTAG)) {
+                            listAdapter.add(result.getDevice().getName());
+                            deviceList.add(result.getDevice());
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    protected BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                Log.i(TAG, "onConnectionStateChange() - STATE_CONNECTED");
+                bluetoothGatt = gatt;
+                timer = new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
+
+                                          @Override
+                                          public void run() {
+                                              //Called each time when 5000 milliseconds (5 seconds) (the period parameter)
+                                              boolean rssiStatus = bluetoothGatt.readRemoteRssi();
+                                              //Start a timer here
+                                          }
+
+                                      },
+//Set how long before to start calling the TimerTask (in milliseconds)
+                        0,
+//Set the amount of time between each execution (in milliseconds)
+                        5000);
+
+
+            } else if (newState == BluetoothGatt.STATE_DISCONNECTED) {
+                Log.i(TAG, "onConnectionStateChange() - STATE_DISCONNECTED");
+                timer.cancel();
+                timer = null;
+            }
+        }
+
+        @Override
+        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status){
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d(TAG, String.format("BluetoothGatt ReadRssi[%d]", rssi));
+                double distance = getDistance(rssi, 1);
+                Log.i(TAG, "Distance is: "+distance);
+                if(distanceList.size() == MAX_DISTANCE_VALUES) {
+                    double sum = 0;
+                    for(int i = 0; i < MAX_DISTANCE_VALUES; i++) {
+                        sum = sum + distanceList.get(i);
+                    }
+                    final double averageDistance = sum / MAX_DISTANCE_VALUES;
+                    distanceList.clear();
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("proximity");
+                    myRef.setValue(new Double(averageDistance).toString());
+                    showToast("iTag is "+averageDistance+" mts. away");
+                } else {
+                    showToast("Gathering Data");
+                    distanceList.add(distance);
+                }
+
+            }
+        }
+    };
+
+    final void showToast(final String message) {
+        runOnUiThread(new Runnable() {
+                          @Override
+                          public void run() {
+                              Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                          }
+                      }
+        );
+    }
+
+    private void initialiseBluetooth() {
+        bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+    }
+
+    private boolean isDuplicate(BluetoothDevice device){
+        for(int i = 0; i < listAdapter.getCount(); i++) {
+            String addedDeviceDetail = listAdapter.getItem(i);
+            if(addedDeviceDetail.equals(device.getAddress()) || addedDeviceDetail.equals(device.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void startScanning() {
+        listAdapter.clear();
+        deviceList.clear();
+        startScanningButton.setVisibility(View.INVISIBLE);
+        stopScanningButton.setVisibility(View.VISIBLE);
+        if (bluetoothLeScanner == null) {
+            initialiseBluetooth();
+        }
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                bluetoothLeScanner.startScan(leScanCallback);
+            }
+        });
+    }
+
+    public void stopScanning() {
+        startScanningButton.setVisibility(View.VISIBLE);
+        stopScanningButton.setVisibility(View.INVISIBLE);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                bluetoothLeScanner.stopScan(leScanCallback);
+            }
+        });
+    }
+
+    double getDistance(int rssi, int txPower) {
+        /*
+         * RSSI = TxPower - 10 * n * lg(d)
+         * n = 2 (in free space)
+         *
+         * d = 10 ^ ((TxPower - RSSI) / (10 * n))
+         */
+        return (Math.pow(10d, ((double) txPower - rssi) / (10 * 4)))/100;
+    }
+}
